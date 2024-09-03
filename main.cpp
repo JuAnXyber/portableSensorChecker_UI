d29ef7fc50394e7e5ba9319138a8a52812f918d6
JuAnXyber <89549253+JuAnXyber@users.noreply.github.com>
2024-09-03 13:33:26 +0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,63 +1,83 @@
 // Learn about the ESP32 WiFi simulation in
 // https://docs.wokwi.com/guides/esp32-wifi
-// btn means the button
 
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+// #include <DHT.h>
+// #include <dht.h>
+#include <DHTesp.h>
 
+DHTesp dhtSensor;
 LiquidCrystal_I2C LCD = LiquidCrystal_I2C(0x27, 16, 2);
 
-#define btn1 16 //define your button pins here
-#define btn2 17 //define your button pins here
-#define btn3 18 //define your button pins here
-#define btn4 2  //define your button pins here
-#define btn5 4  //define your button pins here
+#define btn1 16
+#define btn2 17
+#define btn3 18
+#define btn4 2
+#define btn5 4
+#define relay 19
 
-int page=0;
-String pilihanSensor = "";
-// int tempSlaveId = 0;
-int tempSlaveId = 0;
-int humidSlaveId = 0;
+const int DHT_PIN = 15;
+
+int page           = 0;
+int pilihanSensor  = 0;
+int tempSlaveId    = 0;
+int humidSlaveId   = 0;
 
 void page_pilihSensor() {
   LCD.setCursor(0, 1);
   LCD.println("<Back Next>   OK");
-  if(digitalRead(btn1)==1)
+  if(pilihanSensor == 1) 
+  {
+    LCD.setCursor(0, 0);
+    LCD.println("<  temperature >");
+    Serial.println(pilihanSensor);
+  }
+  else if(pilihanSensor == 2) 
   {
     LCD.setCursor(0, 0);
     LCD.println("<   humidity   >");
-    pilihanSensor = "humidity";
+    Serial.println(pilihanSensor);
+  }
+
+  if(digitalRead(btn1)==1)
+  {
+    pilihanSensor = pilihanSensor+1;
     Serial.println(pilihanSensor);
   }
   else if(digitalRead(btn2)==1)
   {
-    LCD.setCursor(0, 0);
-    LCD.println("< temperature  >");
-    pilihanSensor = "temperature";
+    pilihanSensor = pilihanSensor+1;
     Serial.println(pilihanSensor);
   }
   else if(digitalRead(btn5)==1)
   {
     Serial.println("OK");
     Serial.println(pilihanSensor);
-    if(pilihanSensor=="humidity")
-    {
-      page=1;
-    }
-    else if(pilihanSensor=="temperature")
+    if(pilihanSensor == 1)
     {
       page=2;
     }
+    else if(pilihanSensor == 2)
+    {
+      page=1;
+    }
+  }
+  if(pilihanSensor == 3)
+  {
+    pilihanSensor = 1;
   }
   Serial.println(page);
 }
 
 
 void page_rwSensorHumidity() {
+  TempAndHumidity  data = dhtSensor.getTempAndHumidity();
   LCD.setCursor(0,0);
-  LCD.println("   (humidity)   ");
+  LCD.println("HUMID :" + String(data.humidity, 1) + "%");
   LCD.setCursor(0,1);
   LCD.println("<Back Write>    ");
+  Serial.println("Humidity: " + String(data.humidity, 1) + "%");
   if(digitalRead(btn1)==1)
   {
     page=0;
@@ -70,13 +90,16 @@ void page_rwSensorHumidity() {
 }
 
 void page_rwSensorTemperature() {
+  TempAndHumidity  data = dhtSensor.getTempAndHumidity();
   LCD.setCursor(0,0);
-  LCD.println("  (temperature) ");
+  LCD.println("TEMP :" + String(data.temperature, 2) + "C");
   LCD.setCursor(0,1);
   LCD.println("<Back Write>    ");
+  Serial.println("Temperature: " + String(data.temperature, 2) + "°C");
   if(digitalRead(btn1)==1)
   {
     page=0;
+    pilihanSensor = 1;
     LCD.setCursor(0, 0);
     LCD.println("< PILIH SENSOR >");
   }
@@ -98,6 +121,7 @@ void page_writeSensorHumidity() {
   if(digitalRead(btn1)==1)
   {
     page=0;
+    pilihanSensor = 2;
     LCD.setCursor(0, 0);
     LCD.println("< PILIH SENSOR >");
   }
@@ -113,12 +137,7 @@ void page_writeSensorHumidity() {
   }
   else if(digitalRead(btn5))
   {
-    LCD.setCursor(0,0);
-    LCD.println("Write ID Success");
-    LCD.setCursor(0,1);
-    LCD.println("Wait 5 Second...");
-    delay(5000);
-    page=1;
+    page=5;
   }
   if(humidSlaveId<=0)
   {
@@ -155,13 +174,7 @@ void page_writeSensorTemperature() {
   }
   else if(digitalRead(btn5)==1)
   {
-    LCD.setCursor(0,0);
-    LCD.println("Write ID Success");
-    LCD.setCursor(0,1);
-    LCD.println("Wait 5 Second...");
-    delay(5000);
-    page=2;
-
+    page=6;
   }
   if(tempSlaveId<=0)
   {
@@ -173,6 +186,55 @@ void page_writeSensorTemperature() {
   }
 }
 
+void page_vadidateWriteHumidity() {
+  LCD.setCursor(0,0);
+  LCD.println(" WRITE PROCCESS ");
+  LCD.setCursor(0,1);
+  LCD.println("WAIT FOR 10 SEC ");
+  //VALIDATING 
+  LCD.setCursor(0,0);
+  LCD.println("RESTARTING .....");
+  LCD.setCursor(0,1);
+  LCD.println("PLEASE WAIT ....");
+  delay(5000);
+  digitalWrite(relay, HIGH);
+  Serial.println("relay nyala");
+  delay(5000);
+  Serial.println("relay mati");
+  digitalWrite(relay, LOW);
+  LCD.setCursor(0,0);
+  LCD.println("SUCCESS RESTART ");
+  LCD.setCursor(0,1);
+  LCD.println("WAIT FOR 5 SEC  ");
+  delay(3000);
+  LCD.clear();
+  page=1;
+}
+
+void page_vadidateWriteTemperature() {
+  LCD.setCursor(0,0);
+  LCD.println(" WRITE PROCCESS ");
+  LCD.setCursor(0,1);
+  LCD.println("WAIT FOR 10 SEC ");
+  //VALIDATING 
+  LCD.setCursor(0,0);
+  LCD.println("RESTARTING .....");
+  LCD.setCursor(0,1);
+  LCD.println("PLEASE WAIT ....");
+  delay(5000);
+  digitalWrite(relay, HIGH);
+  delay(5000);
+  digitalWrite(relay, LOW);
+  LCD.setCursor(0,0);
+  LCD.println("SUCCESS RESTART ");
+  LCD.setCursor(0,1);
+  LCD.println("WAIT FOR 5 SEC  ");
+  delay(3000);
+  LCD.clear();
+  page=2;
+}
+
+
 void setup() {
   Serial.begin(115200);
   pinMode(btn1, INPUT);
@@ -180,6 +242,9 @@ void setup() {
   pinMode(btn3, INPUT);
   pinMode(btn4, INPUT);
   pinMode(btn5, INPUT);
+  pinMode(relay, OUTPUT);
+  // pinMode(sensor, INPUT);
+  dhtSensor.setup(DHT_PIN, DHTesp::DHT22);
 
   LCD.init();
   LCD.backlight();
@@ -214,6 +279,15 @@ void loop() {
   {
     page_writeSensorTemperature();
   }
+  if(page==5)
+  {
+    page_vadidateWriteHumidity();
+  }
+  if(page==6)
+  {
+    page_vadidateWriteTemperature();
+  }
 
+  TempAndHumidity  data = dhtSensor.getTempAndHumidity();
   delay(50);
 }
